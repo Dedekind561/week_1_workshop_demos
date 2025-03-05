5ff5297f3d30c4f37b4eaf29a83e57f6f232c28a
MitchLloyd <mitchelllloyd154@gmail.com>
2025-03-05 10:42:42 +0000

    display user name in first user card

--- day-3/script.ts
+++ day-3/script.ts
@@ -4,13 +4,18 @@ import getData from './data.js';
 
 const data = getData();
 const [user] = data;
-console.log(user);
+
 
 // bring in the getData function and check we can access data (done)
 // start creating a user card in the UI
 
 // create a section element
 const section = document.createElement('section');
+
+// create a heading element
+const heading = document.createElement('h2');
+heading.innerText = `Name: ${user.name}`;
+section.appendChild(heading);
 
 // give the section element a class
 section.classList.add('user-card');
