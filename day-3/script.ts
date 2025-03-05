cfb1e11dc5b781d7085046d228b549243db95139
MitchLloyd <mitchelllloyd154@gmail.com>
2025-03-05 10:48:35 +0000

    add user cards to the main gallery

--- day-3/script.ts
+++ day-3/script.ts
@@ -9,8 +9,14 @@ const [user] = data;
 // bring in the getData function and check we can access data (done)
 // start creating a user card in the UI
 
+// retrieve the gallery element
+// append section to the gallery element
+
+const gallery = document.querySelector('.main-gallery');
+
 // create a section element
 const section = document.createElement('section');
+
 
 // create a heading element
 const heading = document.createElement('h2');
@@ -20,5 +26,5 @@ section.appendChild(heading);
 // give the section element a class
 section.classList.add('user-card');
 
-// attach it to the DOM tree
-document.body.appendChild(section);
+// attach user card section to the gallery
+gallery!.appendChild(section);
